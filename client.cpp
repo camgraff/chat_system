8d733afaba13535a7ac95c9d612b324b0c71e23b
cam <camerongraff@mines.edu>
2019-10-08 11:52:06 -0600

    Added client connection. Server and client can connect.

--- client.cpp
+++ client.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <string.h> 
+using namespace std;
 
-int main() {
-    return 0;
+//port number
+#define PORT 8888
+
+int main(int argc, char* argv[]) {
+    //socket setup
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET; 
+    //serverAddr.sin_addr.s_addr = inet_addr(serverAddr);
+    serverAddr.sin_port = htons(PORT); 
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    //connect to the server
+    if (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        cout << "Failed to connect to server. Exiting..." << endl;
+        exit(0);
+    } else {
+        cout << "Connected to server!" <<endl;
+    }
+
+    close(sock);
 }
--- server.cpp
+++ server.cpp
@@ -18,26 +18,39 @@ int main(int argc, char* argv[]) {
 
     //socket setup
     sockaddr_in serverAddr;
+    bzero((char*)&serverAddr, sizeof(serverAddr));      //needed to reset serverAddr on sonsecutive runs
     serverAddr.sin_family = AF_INET; 
-    serverAddr.sin_addr.s_addr = INADDR_ANY; 
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); 
     serverAddr.sin_port = htons(PORT); 
 
     //socket file descriptor
     int serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverfd == -1) {
+        cout << "Error creating server socket. Exiting..." << endl;
+        exit(0);
+    }
 
     //bind the socket
-    bind(serverfd, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    int binding = bind(serverfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (binding == -1) {
+        cout << "Error binding server socket. Exiting..." << endl;
+        exit(0);
+    }
 
     //start listening
     cout << "Listening for client connection..." << endl;
     listen(serverfd, MAX_CLIENTS);
     //accept the client connection
-    int newSocket = accept(serverfd, (sockaddr*)&serverAddr, (socklen_t*) sizeof(serverAddr));
-    if (newSocket == -1) {
-        cout << "Error accepting client request. Exiting..." << endl;
+    sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    int clientSocket = accept(serverfd, (struct sockaddr*)&clientAddr, &clientAddrLen);
+    if (clientSocket == -1) {
+        cout << "Error accepting client request: " << strerror(errno) << " Exiting..." << endl;
         exit(0);
-    } else {
-        cout << "Connected with client!" << endl; 
     }
-    
+    cout << "Connected with client!" << endl; 
+
+
+    //close socket
+    close(serverfd);
 }
