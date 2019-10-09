d9be3e071f4d6622ef42e32b779db24d02a6f5f0
cam <camerongraff@mines.edu>
2019-10-09 08:31:03 -0600

    Server now handles client disconnects properly.

--- server.cpp
+++ server.cpp
@@ -6,6 +6,7 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #include <set>
+#include <map>
 using namespace std;
 
 //port number
@@ -19,26 +20,28 @@ int main(int argc, char* argv[]) {
     char buffer[1024] = {0};
 
     //set of client sockets
-    set<int> clientSockets;       
+    set<int> clientSockets;
+
+    //map of client sockets to user IDs
+    map<int, string> userIDs;      
 
     //set of client socket descriptors (used with select())
     fd_set clientSds;
 
     //socket to recieve client connections
     int recvSocket = socket(AF_INET, SOCK_STREAM, 0);
-    //use this option for testing so we can reuse the same socket quickly
+    //use this option for testing so we can reuse the same socket
     int optval = true;
     setsockopt(recvSocket, SOL_SOCKET, SO_REUSEADDR, (char *)&optval, sizeof(optval));
 
     //socket setup
     sockaddr_in serverAddr;
-    bzero(&serverAddr, sizeof(serverAddr));      //need to reset serverAddr on sonsecutive runs
+    bzero(&serverAddr, sizeof(serverAddr));      //need to reset serverAddr on consecutive runs
     serverAddr.sin_family = AF_INET; 
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); 
     serverAddr.sin_port = htons(PORT); 
 
     //bind the socket
-    //int binding = bind(recvSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
     if (bind(recvSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
         cout << "Error binding server socket [" << strerror(errno) << "]. Exiting..." << endl;
         exit(0);
@@ -79,27 +82,40 @@ int main(int argc, char* argv[]) {
                 } else {
                     //recieve userID from client
                     recv(newSocket, &buffer, sizeof(buffer), 0);
-                    string temp(buffer);
-                    strcpy(buffer, (temp + " connected.").c_str());
+                    string userID(buffer);
+
+                    strcpy(buffer, (userID + " connected.").c_str());
                     cout << buffer << endl; 
-                }
 
-                //push new client socket to set of sockets
-                clientSockets.emplace(newSocket);
+                    //send new client info to all other active clients 
+                    for (int cli : clientSockets) {
+                        send(cli, &buffer, strlen(buffer), 0);
+                    }
 
-                //send new client info to all other active clients 
-                for (int cli : clientSockets) {
-                    send(cli, &buffer, strlen(buffer), 0);
+                    //push new client socket to set of sockets
+                    clientSockets.emplace(newSocket);
+
+                    //add user ID to map
+                    userIDs.emplace(newSocket, userID);
                 }
             }
         } else {
             //otherwise recieve the client message
             for (int cli : clientSockets) {
                 if (FD_ISSET(cli, &clientSds)) {
+                    
                     //clear the buffer
                     memset(&buffer, 0, sizeof(buffer));
-                    recv(cli, &buffer, sizeof(buffer), 0);
+
+                    //if recv returns 0, client has left the room, so close the socket and remove from set and map
+                    if (recv(cli, &buffer, sizeof(buffer), 0) == 0) {
+                        strcpy(buffer, (userIDs[cli] + " disconnected.").c_str());
+                        close(cli);
+                        clientSockets.erase(cli);
+                    }
+                    
                     cout << buffer << endl;
+
                     //send this message to the other clients
                     for (int other : clientSockets) {
                         if (cli != other) 
